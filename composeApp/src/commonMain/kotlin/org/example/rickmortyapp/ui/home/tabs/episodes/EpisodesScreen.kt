cab27b43e5b42a997ef5f619a3ae18f2a6f892f3
dnuccio80 <nuccio227@gmail.com>
2025-07-11 20:23:13 -0300

    - Added LazyRow on EpisodesScreen

--- composeApp/src/commonMain/kotlin/org/example/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
@@ -1,19 +1,19 @@
 package org.example.rickmortyapp.ui.home.tabs.episodes
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -26,15 +26,32 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.paging.LoadState
 import app.cash.paging.compose.collectAsLazyPagingItems
 import org.example.rickmortyapp.core.Logger
 import org.example.rickmortyapp.domain.model.EpisodeModel
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_1
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_2
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_3
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_4
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_5
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_6
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_7
+import org.example.rickmortyapp.domain.model.SeasonNumber.SEASON_8
+import org.example.rickmortyapp.domain.model.SeasonNumber.UNKNOWN
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
+import rickmortyapp.composeapp.generated.resources.Res
+import rickmortyapp.composeapp.generated.resources.season_1
+import rickmortyapp.composeapp.generated.resources.season_2
+import rickmortyapp.composeapp.generated.resources.season_3
+import rickmortyapp.composeapp.generated.resources.season_4
+import rickmortyapp.composeapp.generated.resources.season_5
+import rickmortyapp.composeapp.generated.resources.season_6
+import rickmortyapp.composeapp.generated.resources.season_7
+import rickmortyapp.composeapp.generated.resources.season_8
 
 
 @Composable
@@ -49,16 +66,14 @@ fun EpisodesScreen() {
 
     Scaffold { innerPadding ->
         Box(modifier = Modifier.fillMaxSize().background(Color.DarkGray))
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        LazyRow  (
+            modifier = Modifier.fillMaxWidth().height(350.dp).padding(innerPadding),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             when {
                 // First Loading
                 episodes.loadState.refresh is LoadState.Loading && episodes.itemCount == 0 -> {
-                    item(span = { GridItemSpan(2) }) {
+                    item {
                         Box(
                             modifier = Modifier.fillMaxSize(),
                             contentAlignment = Alignment.Center
@@ -87,7 +102,7 @@ fun EpisodesScreen() {
 
                     // More content to load
                     if (episodes.loadState.append is LoadState.Loading) {
-                        item(span = { GridItemSpan(2) }) {
+                        item {
                             Box(
                                 modifier = Modifier.fillMaxSize(),
                                 contentAlignment = Alignment.Center
@@ -107,40 +122,31 @@ fun EpisodesScreen() {
 
 @Composable
 fun EpisodeListItem(episode: EpisodeModel) {
-    Card(
-        modifier = Modifier.fillMaxWidth().height(200.dp).clickable {
 
+    val image = when(episode.season) {
+        SEASON_1 -> painterResource(Res.drawable.season_1)
+        SEASON_2 -> painterResource(Res.drawable.season_2)
+        SEASON_3 -> painterResource(Res.drawable.season_3)
+        SEASON_4 -> painterResource(Res.drawable.season_4)
+        SEASON_5 -> painterResource(Res.drawable.season_5)
+        SEASON_6 -> painterResource(Res.drawable.season_6)
+        SEASON_7 -> painterResource(Res.drawable.season_7)
+        SEASON_8 -> painterResource(Res.drawable.season_8)
+        UNKNOWN -> painterResource(Res.drawable.season_1)
+    }
+
+    Card(
+        modifier = Modifier.width(200.dp).fillMaxHeight().clickable {
         },
         elevation = CardDefaults.cardElevation(16.dp),
-        shape = RoundedCornerShape(16.dp),
+        shape = RoundedCornerShape(4.dp),
         border = BorderStroke(1.dp, Color.Green.copy(alpha = .6f)),
         colors = CardDefaults.cardColors(
             containerColor = Color.Black
         )
     ) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    modifier = Modifier.padding(16.dp),
-                    text = episode.name,
-                    color = Color.Green.copy(alpha = .8f),
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.ExtraBold,
-                    textAlign = TextAlign.Center
-                )
-                Text(
-                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
-                    text = episode.season.toString(),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    textAlign = TextAlign.Center,
-                    color = Color.White
-                )
-            }
+            Image(image, contentDescription = "", modifier = Modifier.fillMaxSize(), contentScale = ContentScale.FillBounds)
         }
     }
 }
